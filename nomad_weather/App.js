048d88600b9f54f07b5a8621177e78f272c73275
문경림 <naru@takeone.co.kr>
2025-11-24 19:41:37 +0900

    update

--- nomad_weather/App.js
+++ nomad_weather/App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Heldddlo! I made a RN App!</Text>
+      <Text style={styles.text}>Heldo! I made a RN App!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+// StyleSheet.create :: object를 생성하는데 사용함..
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    color: 'blue',
+  }
 });
