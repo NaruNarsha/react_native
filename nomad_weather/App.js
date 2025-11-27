d2330a4803df2aa0d823b14ce0317432ab114d40
문경림 <naru@takeone.co.kr>
2025-11-27 21:01:37 +0900

    udpate nomad_weather

--- nomad_weather/App.js
+++ nomad_weather/App.js
@@ -1,15 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { Ionicons, Fontisto } from "@expo/vector-icons";
 
+//  Dimensions: get device's width, height
+const { height, width: SCREEN_WIDTH } = Dimensions.get("window");
+
+console.log(height, SCREEN_WIDTH);
+
+const API_KEY = "e42a0645a168c6b5c1b8a4f8fe567071";
+
+const icon = {
+  Clouds: "cloudy",
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  // Permission request function
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    //console.log(location);
+    //setCity(location[0].region);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+
+    const json = await response.json();
+
+    console.log(json.daily);
+
+    // setDays(
+    //   json.list.filter((weather) => {
+    //     if (weather.dt_txt.includes("00:00:00")) {
+    //       console.log(weather);
+    //       return weather;
+    //     }
+    //   })
+    // );
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 2, backgroundColor: "tomato"}}></View>
-      <View style={{ flex: 5, backgroundColor: "teal"}}></View>
-      <View style={{ flex: 1, backgroundColor: "orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        //indicatorStyle="white" // only for iOS
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+          <Fontisto name="cloudy" size={68} color="black" />
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 180,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
